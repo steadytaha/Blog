d71bc6fbdd213998f06915fddcf752a46a4c0264
Taha Efe <littleman.teg@gmail.com>
2024-05-23 21:51:48 +0300

    Image uploading functionality is completed.

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -80,7 +80,7 @@ export default function SignIn() {
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Don't have an account?</span>
-            <Link to='/signin' className='text-blue-500'> Sign Up</Link>
+            <Link to='/signup' className='text-blue-500'> Sign Up</Link>
           </div>
           {
             errorMessage && (

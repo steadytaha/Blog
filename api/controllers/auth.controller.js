e537fee5cc7cb8d36dec60b93d866da7267136df
Taha Efe <littleman.teg@gmail.com>
2024-05-26 13:14:48 +0300

    Admin feature is added.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -43,7 +43,7 @@ export const signin = async (req, res, next) => {
             return next(errorHandler(400, "Invalid password"));
         }
         const { password: hashedPassword, ...rest } = validUser._doc;
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET, { expiresIn: "1d" });
         res.status(200).cookie('token', token, { httpOnly: true }).json(rest);
     } catch (error) {
         next(error);
@@ -55,7 +55,7 @@ export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({email});
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: "1d" });
             const {password, ...rest} = user._doc;
             res.status(200).cookie('token', token, { httpOnly: true }).json(rest);
         } else {
@@ -65,10 +65,11 @@ export const google = async (req, res, next) => {
                 username: name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),
                 email,
                 password: hashedPassword,
-                photo
+                photo,
+                isAdmin
             });
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET, { expiresIn: "1d" });
             const {password, ...rest} = newUser._doc;
             res.status(200).cookie('token', token, { httpOnly: true }).json(rest);
         }

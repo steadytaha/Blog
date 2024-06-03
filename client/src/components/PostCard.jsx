56a56168c55f7304b8e30f809799da0ba23b756f
Taha Efe <littleman.teg@gmail.com>
2024-06-03 22:12:06 +0300

    Color arrangements. v1.1.2

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -15,7 +15,7 @@ export default function PostCard({ post }) {
         <span className='italic text-sm'>{post.category}</span>
         <Link
           to={`/post/${post.slug}`}
-          className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border dark:border-teal-500 dark:text-teal-500 dark:hover:bg-teal-500 border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
+          className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border dark:border-teal-500 dark:text-teal-100 dark:hover:bg-teal-500 border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
         >
           Read article
         </Link>

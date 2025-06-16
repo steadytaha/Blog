4945fd415b9b70b0371101a081615066bab418cd
Taha Efe <littleman.teg@gmail.com>
2025-06-16 23:59:02 +0300

    Fixing "new password" changing issue.

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,22 +6,22 @@ export default defineConfig({
   server: {
     proxy: {
       '/auth': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       },
       '/user': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       },
       '/post': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       },
       '/comment': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       }

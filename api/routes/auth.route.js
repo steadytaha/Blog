03e8cc757cb36f1227258b3160f726d0d1f1ee98
Taha Efe <littleman.teg@gmail.com>
2025-06-16 22:01:37 +0300

    Unauthorized error fixed.

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import { signup, signin, google } from '../controllers/auth.controller.js';
 import { validateSignup, validateSignin, validateGoogleAuth } from '../utils/validation.js';
-import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.post('/signup', validateSignup, verifyUser, signup);
-router.post('/signin', validateSignin, verifyUser, signin);
-router.post('/google', validateGoogleAuth, verifyUser, google);
+router.post('/signup', validateSignup, signup);
+router.post('/signin', validateSignin, signin);
+router.post('/google', validateGoogleAuth, google);
 
 export default router;
